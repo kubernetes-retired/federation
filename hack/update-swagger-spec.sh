eparis/kubernetes

Convert hack/ from --flag_name= to --flag-name=

hack/update-swagger-spec.sh
@@ -48,12 +48,12 @@ KUBELET_PORT=${KUBELET_PORT:-10250}
 kube::log::status "Starting kube-apiserver"
 KUBE_API_VERSIONS="v1,v1beta3" "${KUBE_OUTPUT_HOSTBIN}/kube-apiserver" \
   --address="127.0.0.1" \
-  --public_address_override="127.0.0.1" \
+  --public-address-override="127.0.0.1" \
   --port="${API_PORT}" \
-  --etcd_servers="http://${ETCD_HOST}:${ETCD_PORT}" \
-  --public_address_override="127.0.0.1" \
-  --kubelet_port=${KUBELET_PORT} \
-  --runtime_config=api/v1beta3 \
+  --etcd-servers="http://${ETCD_HOST}:${ETCD_PORT}" \
+  --public-address-override="127.0.0.1" \
+  --kubelet-port=${KUBELET_PORT} \
+  --runtime-config=api/v1beta3 \
   --service-cluster-ip-range="10.0.0.0/24" 1>&2 &
 APISERVER_PID=$!
 

kubernetes-retired/federation

hack/update-swagger-spec.sh
@@ -51,12 +51,12 @@ KUBELET_PORT=${KUBELET_PORT:-10250}
 kube::log::status "Starting kube-apiserver"
 KUBE_API_VERSIONS="v1" "${KUBE_OUTPUT_HOSTBIN}/kube-apiserver" \
   --address="127.0.0.1" \
-  --public_address_override="127.0.0.1" \
+  --public-address-override="127.0.0.1" \
   --port="${API_PORT}" \
-  --etcd_servers="http://${ETCD_HOST}:${ETCD_PORT}" \
-  --public_address_override="127.0.0.1" \
-  --kubelet_port=${KUBELET_PORT} \
-  --runtime_config=api/v1 \
+  --etcd-servers="http://${ETCD_HOST}:${ETCD_PORT}" \
+  --public-address-override="127.0.0.1" \
+  --kubelet-port=${KUBELET_PORT} \
+  --runtime-config=api/v1 \
   --service-cluster-ip-range="10.0.0.0/24" >/dev/null 2>&1 &
 APISERVER_PID=$!
 