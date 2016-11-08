kubernetes/kubernetes

Added e2e test for HA master replicas in different zones.
Added e2e test for HA master replicas in different zones.

hack/e2e-internal/e2e-add-master.sh
@@ -16,6 +16,9 @@
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
+if [[ ! -z "${1:-}" ]]; then
+  export GCE_ZONE="${1}"
+fi
 export KUBE_EXPERIMENTAL_REPLICATE_EXISTING_MASTER=true
 
 source "${KUBE_ROOT}/hack/e2e-internal/e2e-up.sh"

hack/e2e-internal/e2e-remove-master.sh
@@ -16,6 +16,9 @@
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
+if [[ ! -z "${1:-}" ]]; then
+  export GCE_ZONE="${1}"
+fi
 export KUBE_DELETE_NODES=false
 
 source "${KUBE_ROOT}/hack/e2e-internal/e2e-down.sh"

kubernetes-retired/federation

hack/e2e-internal/e2e-add-master.sh
@@ -16,6 +16,9 @@
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
+if [[ ! -z "${1:-}" ]]; then
+  export KUBE_GCE_ZONE="${1}"
+fi
 export KUBE_REPLICATE_EXISTING_MASTER=true
 
 source "${KUBE_ROOT}/hack/e2e-internal/e2e-up.sh"

hack/e2e-internal/e2e-remove-master.sh
@@ -16,6 +16,9 @@
 
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
+if [[ ! -z "${1:-}" ]]; then
+  export KUBE_GCE_ZONE="${1}"
+fi
 export KUBE_DELETE_NODES=false
 
 source "${KUBE_ROOT}/hack/e2e-internal/e2e-down.sh"