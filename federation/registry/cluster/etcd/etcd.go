kubernetes/kubernetes

Refactor REST storage to use generic defaults
Signed-off-by: Monis Khan <mkhan@redhat.com>

federation/registry/cluster/etcd/etcd.go
@@ -21,10 +21,11 @@ import (
 	"k8s.io/kubernetes/federation/registry/cluster"
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/rest"
-	"k8s.io/kubernetes/pkg/registry/generic"
+	"k8s.io/kubernetes/pkg/genericapiserver"
 	"k8s.io/kubernetes/pkg/registry/generic/registry"
 	"k8s.io/kubernetes/pkg/runtime"
 	"k8s.io/kubernetes/pkg/storage"
+	"k8s.io/kubernetes/pkg/util/restoptions"
 )
 
 type REST struct {
@@ -45,45 +46,22 @@ func (r *StatusREST) Update(ctx api.Context, name string, objInfo rest.UpdatedOb
 }
 
 // NewREST returns a RESTStorage object that will work against clusters.
-func NewREST(opts generic.RESTOptions) (*REST, *StatusREST) {
-	prefix := "/" + opts.ResourcePrefix
-
-	newListFunc := func() runtime.Object { return &federation.ClusterList{} }
-	storageInterface, _ := opts.Decorator(
-		opts.StorageConfig,
-		100,
-		&federation.Cluster{},
-		prefix,
-		cluster.Strategy,
-		newListFunc,
-		storage.NoTriggerPublisher,
-	)
-
+func NewREST(optsGetter genericapiserver.RESTOptionsGetter) (*REST, *StatusREST) {
 	store := &registry.Store{
 		NewFunc:     func() runtime.Object { return &federation.Cluster{} },
-		NewListFunc: newListFunc,
-		KeyRootFunc: func(ctx api.Context) string {
-			return prefix
-		},
-		KeyFunc: func(ctx api.Context, name string) (string, error) {
-			return registry.NoNamespaceKeyFunc(ctx, prefix, name)
-		},
+		NewListFunc: func() runtime.Object { return &federation.ClusterList{} },
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*federation.Cluster).Name, nil
 		},
-		PredicateFunc:           cluster.MatchCluster,
-		QualifiedResource:       federation.Resource("clusters"),
-		EnableGarbageCollection: opts.EnableGarbageCollection,
-		DeleteCollectionWorkers: opts.DeleteCollectionWorkers,
+		PredicateFunc:     cluster.MatchCluster,
+		QualifiedResource: federation.Resource("clusters"),
 
-		CreateStrategy: cluster.Strategy,
-		UpdateStrategy: cluster.Strategy,
-		DeleteStrategy: cluster.Strategy,
-
+		CreateStrategy:      cluster.Strategy,
+		UpdateStrategy:      cluster.Strategy,
+		DeleteStrategy:      cluster.Strategy,
 		ReturnDeletedObject: true,
-
-		Storage: storageInterface,
 	}
+	restoptions.ApplyOptions(optsGetter, store, storage.NoTriggerPublisher)
 
 	statusStore := *store
 	statusStore.UpdateStrategy = cluster.StatusStrategy

kubernetes-retired/federation

federation/registry/cluster/etcd/etcd.go
@@ -24,7 +24,6 @@ import (
 	"k8s.io/kubernetes/pkg/registry/generic"
 	genericregistry "k8s.io/kubernetes/pkg/registry/generic/registry"
 	"k8s.io/kubernetes/pkg/runtime"
-	"k8s.io/kubernetes/pkg/storage"
 )
 
 type REST struct {
@@ -45,45 +44,24 @@ func (r *StatusREST) Update(ctx api.Context, name string, objInfo rest.UpdatedOb
 }
 
 // NewREST returns a RESTStorage object that will work against clusters.
-func NewREST(opts generic.RESTOptions) (*REST, *StatusREST) {
-	prefix := "/" + opts.ResourcePrefix
-
-	newListFunc := func() runtime.Object { return &federation.ClusterList{} }
-	storageInterface, _ := opts.Decorator(
-		opts.StorageConfig,
-		100,
-		&federation.Cluster{},
-		prefix,
-		cluster.Strategy,
-		newListFunc,
-		cluster.GetAttrs,
-		storage.NoTriggerPublisher,
-	)
-
+func NewREST(optsGetter generic.RESTOptionsGetter) (*REST, *StatusREST) {
 	store := &genericregistry.Store{
 		NewFunc:     func() runtime.Object { return &federation.Cluster{} },
-		NewListFunc: newListFunc,
-		KeyRootFunc: func(ctx api.Context) string {
-			return prefix
-		},
-		KeyFunc: func(ctx api.Context, name string) (string, error) {
-			return genericregistry.NoNamespaceKeyFunc(ctx, prefix, name)
-		},
+		NewListFunc: func() runtime.Object { return &federation.ClusterList{} },
 		ObjectNameFunc: func(obj runtime.Object) (string, error) {
 			return obj.(*federation.Cluster).Name, nil
 		},
-		PredicateFunc:           cluster.MatchCluster,
-		QualifiedResource:       federation.Resource("clusters"),
-		EnableGarbageCollection: opts.EnableGarbageCollection,
-		DeleteCollectionWorkers: opts.DeleteCollectionWorkers,
+		PredicateFunc:     cluster.MatchCluster,
+		QualifiedResource: federation.Resource("clusters"),
 
-		CreateStrategy: cluster.Strategy,
-		UpdateStrategy: cluster.Strategy,
-		DeleteStrategy: cluster.Strategy,
-
+		CreateStrategy:      cluster.Strategy,
+		UpdateStrategy:      cluster.Strategy,
+		DeleteStrategy:      cluster.Strategy,
 		ReturnDeletedObject: true,
-
-		Storage: storageInterface,
+	}
+	options := &generic.StoreOptions{RESTOptions: optsGetter, AttrFunc: cluster.GetAttrs}
+	if err := store.CompleteWithOptions(options); err != nil {
+		panic(err) // TODO: Propagate error up
 	}
 
 	statusStore := *store