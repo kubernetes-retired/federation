irfanurrehman/kubernetes

[Federation] Register autoscaling apis to federation api server

test/integration/federation/server_test.go
@@ -31,6 +31,7 @@ import (
 	"k8s.io/kubernetes/federation/cmd/federation-apiserver/app"
 	"k8s.io/kubernetes/federation/cmd/federation-apiserver/app/options"
 	"k8s.io/kubernetes/pkg/api/v1"
+	autoscaling_v1 "k8s.io/kubernetes/pkg/apis/autoscaling/v1"
 	ext_v1b1 "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
 	metav1 "k8s.io/kubernetes/pkg/apis/meta/v1"
 	"k8s.io/kubernetes/pkg/runtime/schema"
@@ -42,6 +43,7 @@ var serverIP = fmt.Sprintf("http://localhost:%v", insecurePort)
 var groupVersions = []schema.GroupVersion{
 	fed_v1b1.SchemeGroupVersion,
 	ext_v1b1.SchemeGroupVersion,
+	autoscaling_v1.SchemeGroupVersion,
 }
 
 func TestRun(t *testing.T) {
@@ -205,6 +207,7 @@ func testAPIResourceList(t *testing.T) {
 	testFederationResourceList(t)
 	testCoreResourceList(t)
 	testExtensionsResourceList(t)
+	testAutoscalingResourceList(t)
 }
 
 func testFederationResourceList(t *testing.T) {
@@ -337,4 +340,30 @@ func testExtensionsResourceList(t *testing.T) {
 	assert.NotNil(t, found)
 	assert.True(t, found.Namespaced)
 	found = findResource(apiResourceList.APIResources, "deployments/rollback")
+}
+
+func testAutoscalingResourceList(t *testing.T) {
+	serverURL := serverIP + "/apis/" + autoscaling_v1.SchemeGroupVersion.String()
+	contents, err := readResponse(serverURL)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	var apiResourceList unversioned.APIResourceList
+	err = json.Unmarshal(contents, &apiResourceList)
+	if err != nil {
+		t.Fatalf("Error in unmarshalling response from server %s: %v", serverURL, err)
+	}
+	// empty APIVersion for extensions group
+	assert.Equal(t, "v1", apiResourceList.APIVersion)
+	assert.Equal(t, autoscaling_v1.SchemeGroupVersion.String(), apiResourceList.GroupVersion)
+	// Assert that there are exactly this number of resources.
+	assert.Equal(t, 2, len(apiResourceList.APIResources))
+
+	// Verify hpa
+	found := findResource(apiResourceList.APIResources, "horizontalpodautoscaler")
+	assert.NotNil(t, found)
+	assert.True(t, found.Namespaced)
+	found = findResource(apiResourceList.APIResources, "horizontalpodautoscaler/status")
+	assert.NotNil(t, found)
+	assert.True(t, found.Namespaced)
 }

kubernetes-retired/federation

test/integration/federation/server_test.go
@@ -33,6 +33,7 @@ import (
 	"k8s.io/kubernetes/federation/cmd/federation-apiserver/app"
 	"k8s.io/kubernetes/federation/cmd/federation-apiserver/app/options"
 	"k8s.io/kubernetes/pkg/api/v1"
+	autoscaling_v1 "k8s.io/kubernetes/pkg/apis/autoscaling/v1"
 	batch_v1 "k8s.io/kubernetes/pkg/apis/batch/v1"
 	ext_v1b1 "k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
 )
@@ -44,6 +45,7 @@ var groupVersions = []schema.GroupVersion{
 	fed_v1b1.SchemeGroupVersion,
 	ext_v1b1.SchemeGroupVersion,
 	batch_v1.SchemeGroupVersion,
+	autoscaling_v1.SchemeGroupVersion,
 }
 
 func TestRun(t *testing.T) {
@@ -214,6 +216,7 @@ func testAPIResourceList(t *testing.T) {
 	testCoreResourceList(t)
 	testExtensionsResourceList(t)
 	testBatchResourceList(t)
+	testAutoscalingResourceList(t)
 }
 
 func testFederationResourceList(t *testing.T) {
@@ -370,6 +373,32 @@ func testBatchResourceList(t *testing.T) {
 	assert.NotNil(t, found)
 	assert.True(t, found.Namespaced)
 	found = findResource(apiResourceList.APIResources, "jobs/status")
+	assert.NotNil(t, found)
+	assert.True(t, found.Namespaced)
+}
+
+func testAutoscalingResourceList(t *testing.T) {
+	serverURL := serverIP + "/apis/" + autoscaling_v1.SchemeGroupVersion.String()
+	contents, err := readResponse(serverURL)
+	if err != nil {
+		t.Fatalf("%v", err)
+	}
+	var apiResourceList metav1.APIResourceList
+	err = json.Unmarshal(contents, &apiResourceList)
+	if err != nil {
+		t.Fatalf("Error in unmarshalling response from server %s: %v", serverURL, err)
+	}
+	// empty APIVersion for extensions group
+	assert.Equal(t, "v1", apiResourceList.APIVersion)
+	assert.Equal(t, autoscaling_v1.SchemeGroupVersion.String(), apiResourceList.GroupVersion)
+	// Assert that there are exactly this number of resources.
+	assert.Equal(t, 2, len(apiResourceList.APIResources))
+
+	// Verify hpa
+	found := findResource(apiResourceList.APIResources, "horizontalpodautoscalers")
+	assert.NotNil(t, found)
+	assert.True(t, found.Namespaced)
+	found = findResource(apiResourceList.APIResources, "horizontalpodautoscalers/status")
 	assert.NotNil(t, found)
 	assert.True(t, found.Namespaced)
 }