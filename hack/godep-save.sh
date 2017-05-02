deads2k/kubernetes

wire new staging repo

hack/godep-save.sh
@@ -50,6 +50,9 @@ pushd "${KUBE_ROOT}" > /dev/null
   if [ ! -e "vendor/k8s.io/kube-aggregator" ]; then
     ln -s ../../staging/src/k8s.io/kube-aggregator vendor/k8s.io/kube-aggregator
   fi
+  if [ ! -e "vendor/k8s.io/kube-apiextension-server" ]; then
+    ln -s ../../staging/src/k8s.io/kube-apiextension-server vendor/k8s.io/kube-apiextension-server
+  fi
   if [ ! -e "vendor/k8s.io/sample-apiserver" ]; then
     ln -s ../../staging/src/k8s.io/sample-apiserver vendor/k8s.io/sample-apiserver
   fi

hack/lib/golang.sh
@@ -30,6 +30,7 @@ kube::golang::server_targets() {
     cmd/kubeadm
     cmd/hyperkube
     vendor/k8s.io/kube-aggregator
+    vendor/k8s.io/kube-apiextension-server
     plugin/cmd/kube-scheduler
   )
   echo "${targets[@]}"

hack/make-rules/test.sh
@@ -72,6 +72,9 @@ kube::test::find_dirs() {
     find ./staging/src/k8s.io/kube-aggregator -name '*_test.go' \
       -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
 
+    find ./staging/src/k8s.io/kube-apiextension-server -name '*_test.go' \
+      -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
+
     find ./staging/src/k8s.io/sample-apiserver -name '*_test.go' \
       -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
   )

hack/update-codegen.sh
@@ -102,3 +102,4 @@ ${informergen} \
 
 # call generation on sub-project for now
 vendor/k8s.io/kube-aggregator/hack/update-codegen.sh
+vendor/k8s.io/kube-apiextension-server/hack/update-codegen.sh

hack/verify-codegen.sh
@@ -25,6 +25,7 @@ kube::golang::setup_env
 
 # call verify on sub-project for now
 vendor/k8s.io/kube-aggregator/hack/verify-codegen.sh
+vendor/k8s.io/kube-apiextension-server/hack/verify-codegen.sh
 
 "${KUBE_ROOT}/hack/update-codegen.sh" --verify-only
 

kubernetes-retired/federation

hack/godep-save.sh
@@ -50,6 +50,9 @@ pushd "${KUBE_ROOT}" > /dev/null
   if [ ! -e "vendor/k8s.io/kube-aggregator" ]; then
     ln -s ../../staging/src/k8s.io/kube-aggregator vendor/k8s.io/kube-aggregator
   fi
+  if [ ! -e "vendor/k8s.io/kube-apiextensions-server" ]; then
+    ln -s ../../staging/src/k8s.io/kube-apiextensions-server vendor/k8s.io/kube-apiextensions-server
+  fi
   if [ ! -e "vendor/k8s.io/sample-apiserver" ]; then
     ln -s ../../staging/src/k8s.io/sample-apiserver vendor/k8s.io/sample-apiserver
   fi

hack/lib/golang.sh
@@ -30,6 +30,7 @@ kube::golang::server_targets() {
     cmd/kubeadm
     cmd/hyperkube
     vendor/k8s.io/kube-aggregator
+    vendor/k8s.io/kube-apiextensions-server
     plugin/cmd/kube-scheduler
   )
   echo "${targets[@]}"

hack/make-rules/test.sh
@@ -72,6 +72,9 @@ kube::test::find_dirs() {
     find ./staging/src/k8s.io/kube-aggregator -name '*_test.go' \
       -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
 
+    find ./staging/src/k8s.io/kube-apiextensions-server -name '*_test.go' \
+      -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
+
     find ./staging/src/k8s.io/sample-apiserver -name '*_test.go' \
       -name '*_test.go' -print0 | xargs -0n1 dirname | sed 's|^\./staging/src/|./vendor/|' | LC_ALL=C sort -u
   )

hack/update-codegen.sh
@@ -103,3 +103,4 @@ ${informergen} \
 # call generation on sub-project for now
 vendor/k8s.io/kube-aggregator/hack/update-codegen.sh
 vendor/k8s.io/sample-apiserver/hack/update-codegen.sh
+vendor/k8s.io/kube-apiextensions-server/hack/update-codegen.sh

hack/verify-codegen.sh
@@ -26,6 +26,7 @@ kube::golang::setup_env
 # call verify on sub-project for now
 vendor/k8s.io/kube-aggregator/hack/verify-codegen.sh
 vendor/k8s.io/sample-apiserver/hack/verify-codegen.sh
+vendor/k8s.io/kube-apiextensions-server/hack/verify-codegen.sh
 
 "${KUBE_ROOT}/hack/update-codegen.sh" --verify-only
 