9b9c86c05f93fff0d60c8c50f08194fd4f4b9d58
Irfan Ur Rehman <irfan.rehman@huawei.com>
2017-10-24 12:53:47 +0530

    Fixup kubectl and kubernetes paths

--- deploy/cluster/log-dump.sh
+++ deploy/cluster/log-dump.sh
@@ -22,6 +22,7 @@ set -o nounset
 set -o pipefail
 
 # For FEDERATION_NAMESPACE
+REAL_KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../../../kubernetes
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 source "${KUBE_ROOT}/deploy/cluster/common.sh"
 
@@ -30,7 +31,7 @@ OUTPUT_DIR="${REPORT_DIR}/federation"
 
 # Dumps logs for all pods in a federation.
 function dump_federation_pod_logs() {
-  local -r federation_pod_names_string="$(kubectl get pods -l 'app=federated-cluster' --namespace=${FEDERATION_NAMESPACE} -o name)"
+  local -r federation_pod_names_string="$(${REAL_KUBE_ROOT}/cluster/kubectl.sh get pods -l 'app=federated-cluster' --namespace=${FEDERATION_NAMESPACE} -o name)"
   if [[ -z "${federation_pod_names_string}" ]]; then
     return
   fi
@@ -43,7 +44,7 @@ function dump_federation_pod_logs() {
       continue
     fi
 
-    kubectl logs "${pod_name}" --namespace="${FEDERATION_NAMESPACE}" \
+    ${REAL_KUBE_ROOT}/cluster/kubectl.sh logs "${pod_name}" --namespace="${FEDERATION_NAMESPACE}" \
         >"${OUTPUT_DIR}/${pod_name#pods/}.log"
   done
 }
@@ -54,7 +55,7 @@ function dump_federation_pod_logs() {
 function dump_apiserver_pod_logs() {
   local -r apiserver_pod_containers=(apiserver etcd)
   for container in ${apiserver_pod_containers[@]}; do
-    kubectl logs "${1}" -c "${container}" --namespace="${FEDERATION_NAMESPACE}" \
+    ${REAL_KUBE_ROOT}/cluster/kubectl.sh logs "${1}" -c "${container}" --namespace="${FEDERATION_NAMESPACE}" \
         >"${OUTPUT_DIR}/${1#pods/}-${container}.log"
   done
 }
@@ -63,7 +64,7 @@ function dump_apiserver_pod_logs() {
 # TODO: This currently only grabs DNS pod logs from the host cluster. It should
 # grab those logs from all clusters in the federation.
 function dump_dns_pod_logs() {
-  local -r dns_pod_names_string="$(kubectl get pods -l 'k8s-app=kube-dns' --namespace=kube-system -o name)"
+  local -r dns_pod_names_string="$(${REAL_KUBE_ROOT}/cluster/kubectl.sh get pods -l 'k8s-app=kube-dns' --namespace=kube-system -o name)"
   if [[ -z "${dns_pod_names_string}" ]]; then
     return
   fi
@@ -75,7 +76,7 @@ function dump_dns_pod_logs() {
     # As of 3/2017, the only pod that matches the kube-dns label is kube-dns, and
     # it has three containers.
     for container in ${dns_pod_containers[@]}; do
-      kubectl logs "${pod_name}" -c "${container}" --namespace=kube-system \
+      ${REAL_KUBE_ROOT}/cluster/kubectl.sh logs "${pod_name}" -c "${container}" --namespace=kube-system \
           >"${OUTPUT_DIR}/${pod_name#pods/}-${container}.log"
     done
   done
