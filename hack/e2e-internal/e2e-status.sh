53a48c5963cf0aec8da91812adc6ee7ffb5a308a
Irfan Ur Rehman <irfan.rehman@huawei.com>
2017-11-07 00:20:46 +0530

    Update scripts to enable k8s cluster bringup from federation repo

--- hack/e2e-internal/e2e-status.sh
+++ hack/e2e-internal/e2e-status.sh
@@ -18,9 +18,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+REAL_KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../../../kubernetes
 KUBE_ROOT=$(dirname "${BASH_SOURCE}")/../..
 
-: ${KUBECTL:=${KUBE_ROOT}/cluster/kubectl.sh}
+: ${KUBECTL:=${REAL_KUBE_ROOT}/cluster/kubectl.sh}
 : ${KUBE_CONFIG_FILE:="config-test.sh"}
 
 export KUBECTL KUBE_CONFIG_FILE
